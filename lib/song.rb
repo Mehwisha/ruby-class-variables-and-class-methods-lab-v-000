7879aebf379b509faab46dac706e0e8e008a8921
Mehwish Aziz <mehwisha@msn.com>
2018-05-10 03:04:58 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  attr_accessor :name, :artist, :genre
+ @@count = 0
+ @@artists = []
+ @@genres = []
+# @@artist_count
+# @@genre_count
+
+def initialize(name, artist, genre)
+@name = name
+self.artist = artist
+self.genre = genre
+@@count += 1
+@@genres << genres
+@@artists << artists
+end
+
+
+ def self.count
+@@count
+ end
+ def genres
+  genre.squeeze
+
+end
+def artists
+  artist.squeeze
+end
+# def artist_count
+# @@artists << artist
+# end
+
+end
